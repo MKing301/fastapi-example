fc77a6d0ab03b4013f2687c3d6c700a446df515d
MKING301 <33675411+MKing301@users.noreply.github.com>
2024-07-05 16:36:17 -0400

    Add visit model

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import date
 from fastapi import FastAPI
-from sqlalchemy import Column, Integer, String, Date, Float
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Float
 from database import Base
 
 
@@ -10,3 +10,14 @@ class Person(Base):
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String)
     last_name = Column(String)
+
+
+class Visit(Base):
+    __tablename__ = "visit"
+
+    id = Column(Integer, primary_key=True, index=True)
+    person_id = Column(Integer, ForeignKey("person.id"))
+    arrival_date = Column(Date)
+    departure_date = Column(Date)
+    duration = Column(Integer)
+    comment = Column(String)
